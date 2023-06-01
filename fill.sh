19809ab228dd0eb4abacf6ac2f135e5334c8da10
Braunerr <mathn519@gmail.com>
2023-06-01 13:05:36 +0200

    line endings

--- fill.sh
+++ fill.sh
@@ -1,4 +1,1 @@
-echo "[*] Running fill.sh script"
-vault kv put secret/mongoSecrets ConnectionString=mongodb+srv://GroenOlsen:BhvQmiihJWiurl2V@auktionshusgo.yzctdhc.mongodb.net/?retryWrites=true&w=majority
-vault kv put secret/JWT Secret=266BABE7C9DB9A34A7EB26E867FB2CA8833303E2CE6AAF28F6DDF04950718141 Issuer=issuer
-vault kv get -mount=secret mongoSecrets
+vault kv put secret/mongoSecrets ConnectionString=mongodb+srv://GroenOlsen:BhvQmiihJWiurl2V@auktionshusgo.yzctdhc.mongodb.net/?retryWrites=true&w=majority vault kv put secret/JWT Secret=266BABE7C9DB9A34A7EB26E867FB2CA8833303E2CE6AAF28F6DDF04950718141 Issuer=issuer vault kv get -mount=secret mongoSecrets
